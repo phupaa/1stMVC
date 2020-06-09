27383b3627fc074771ff782ff05bfb28d206f257
Phupa A <phupaa@hotmail.com>
2020-06-09 11:18:38 +0000

    first

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace ASPNetApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
